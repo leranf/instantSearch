214d1e8561603b1a8558f6d5d49277a08d3719ba
Leran Firer <leranf@gmail.com>
2016-12-13 12:59:39 -0800

    updates script to remove certain payment options and create a json file to upload

--- resources/dataset/dataScript.js
+++ resources/dataset/dataScript.js
@@ -7,9 +7,33 @@ restaurants_info.forEach(function(record) {
   var match = restaurants_list.find(function(rest) {
     return rest.objectID === record.objectID;
   });
-  
+
+  var onlyCertainCards = [];
+  match.payment_options.forEach(function(option) {
+    if (option === 'AMEX' || option === 'Visa' || option === 'MasterCard' || option === 'Discover') {
+      onlyCertainCards.push(option);
+    } else {
+      if (option === 'Diners Club' || option === 'Carte Blanche') {
+        if (match.payment_options.indexOf('Discover') === -1) {
+          onlyCertainCards.push('Discover');
+        }
+      }
+    }
+  });
+
+  match.payment_options = onlyCertainCards;
   record = Object.assign(record, match);
 });
 
-restaurants_info = JSON.stringify(restaurants_info);
-console.log(restaurants_info); // results were copied into restaurants_list_full.json and then uploaded via the dashboard
+// send data to algolia
+// var algoliasearch = require('algoliasearch');
+// var client = algoliasearch("R5FNFOXMUS", "bb54ffbf3bb6805fc86ebed846cff7ca");
+// var index = client.initIndex('new_index');
+// index.addObjects(restaurants_info, function(err, content) {
+//   console.log(content);
+// });
+
+// create json file to uplaod
+// var fs = require('fs');
+// restaurants_info = JSON.stringify(restaurants_info);
+// fs.writeFile('formattedData.json', restaurants_info);
